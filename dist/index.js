a4c0df3d019bc3b62cc2b11d1a9b2bffa102d7fc
sasquatchdev <info@sasquatchdev.me>
2024-10-09 18:18:22 +0200

    Patch 0.2.1
    Removed dist/ folder from vcs

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { getConfig, getDotenvConfig } from "./config.js";
-getDotenvConfig();
-const config = await getConfig();
-console.log("Logging: " + config.debug.level);
-//# sourceMappingURL=index.js.map
